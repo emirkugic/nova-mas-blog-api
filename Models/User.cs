642ffa765cd015ec91556435b7228be8a1d999b1
emirkugic <emir.kugic@hotmail.com>
2024-06-01 21:36:15 +0200

    UPDATE: Updated User model to remove security questions

--- Models/User.cs
+++ Models/User.cs
@@ -1,55 +1,47 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using nova_mas_blog_api.Enums;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace nova_mas_blog_api.Models
 {
+
     public class User
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
 
-        [BsonElement("firstName"), Required, StringLength(100, MinimumLength = 1)]
+        [Required, StringLength(100, MinimumLength = 2)]
         public required string FirstName { get; set; }
 
-        [BsonElement("lastName"), Required, StringLength(100, MinimumLength = 1)]
+        [Required, StringLength(100, MinimumLength = 2)]
         public required string LastName { get; set; }
 
-        [BsonElement("username"), Required, StringLength(100, MinimumLength = 3)]
+        [Required, StringLength(100, MinimumLength = 3)]
         public required string Username { get; set; }
 
-        [BsonElement("email"), Required, EmailAddress]
+        [Required, EmailAddress]
         public required string Email { get; set; }
 
-        [BsonElement("passwordHash"), Required]
+        [Required]
         public required string PasswordHash { get; set; }
 
-        [BsonElement("role"), Required]
-        public required UserRole Role { get; set; }
+        [Required]
+        public UserRole Role { get; set; }
 
-        [BsonElement("isEmailConfirmed")]
-        public required bool IsEmailConfirmed { get; set; }
+        public bool IsEmailConfirmed { get; set; }
 
-        [BsonElement("twoFactorEnabled")]
-        public required bool TwoFactorEnabled { get; set; }
+        public bool TwoFactorEnabled { get; set; }
 
-        [BsonElement("twoFactorSecret")]
         public required string TwoFactorSecret { get; set; }
 
-        [BsonElement("securityQuestions")]
-        public required List<string> SecurityQuestions { get; set; }
-
-        [BsonElement("profilePictureUrl")]
         public required string ProfilePictureUrl { get; set; }
 
-        [BsonElement("createdAt")]
-        public DateTime CreatedAt { get; private set; }
+        public DateTime CreatedAt { get; set; }
 
-        [BsonElement("updatedAt")]
         public DateTime UpdatedAt { get; set; }
     }
-
 }
-
