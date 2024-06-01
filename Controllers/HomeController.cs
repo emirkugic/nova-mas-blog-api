d6832faa3b58db95d049e50c2f98cabea33098ca
emirkugic <emir.kugic@hotmail.com>
2024-06-01 20:18:26 +0200

    FEATURE: Root url has a return message

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace nova_mas_blog_api.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return Content("Welcome to the Nova Mas Blog API. Use /swagger to access the API documentation.");
+            // return Redirect("/swagger");
+        }
+    }
+}
