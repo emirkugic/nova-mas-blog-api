23ee002c9678a97fd31b243c99722874e829d002
emirkugic <emir.kugic@hotmail.com>
2024-06-02 21:14:55 +0200

    FEATURE: Blog Controller with CRUD functionalities

---
+++ Controllers/BlogsController.cs
@@ -0,0 +1,125 @@
+using Microsoft.AspNetCore.Mvc;
+using nova_mas_blog_api.Services;
+using nova_mas_blog_api.Models;
+using nova_mas_blog_api.DTOs;
+using nova_mas_blog_api.DTOs.BlogDTOs;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogsController : ControllerBase
+{
+    private readonly BlogService _blogService;
+    private readonly UserService _userService;
+
+    public BlogsController(BlogService blogService, UserService userService)
+    {
+        _blogService = blogService;
+        _userService = userService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllBlogs(int page = 1, int pageSize = 10)
+    {
+        var blogs = await _blogService.GetAll(page, pageSize);
+        return Ok(blogs);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetBlog(string id)
+    {
+        var blog = await _blogService.GetById(id);
+        if (blog == null) return NotFound();
+
+        var user = await _userService.GetById(blog.user_id);
+        string fullName = user != null ? $"{user.FirstName} {user.LastName}" : "Unknown User";
+
+        var blogDto = new BlogReadDTO
+        {
+            Id = blog.Id!,
+            Title = blog.Title,
+            Content = blog.Content,
+            ImageUrls = blog.ImageUrls,
+            VideoUrls = blog.VideoUrls,
+            Category = blog.Category,
+            DateCreated = blog.DateCreated,
+            ViewCount = blog.ViewCount,
+            IsFeatured = blog.IsFeatured,
+            FullName = fullName
+        };
+
+        return Ok(blogDto);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateBlog([FromBody] BlogCreateDTO blogDto)
+    {
+        var blog = new Blog
+        {
+            Title = blogDto.Title,
+            Content = blogDto.Content,
+            ImageUrls = blogDto.ImageUrls,
+            VideoUrls = blogDto.VideoUrls,
+            Category = blogDto.Category,
+            user_id = blogDto.UserId,
+            DateCreated = DateTime.UtcNow,
+            ViewCount = 0,
+            IsFeatured = false
+        };
+        await _blogService.Create(blog);
+        return CreatedAtAction(nameof(GetBlog), new { id = blog.Id }, blog);
+    }
+
+
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateBlog(string id, [FromBody] BlogUpdateDTO blogDto)
+    {
+        var blog = await _blogService.GetById(id);
+        if (blog == null) return NotFound();
+
+        blog.Title = blogDto.Title;
+        blog.Content = blogDto.Content;
+        blog.ImageUrls = blogDto.ImageUrls;
+        blog.VideoUrls = blogDto.VideoUrls;
+        blog.Category = blogDto.Category;
+
+        await _blogService.Update(id, blog);
+        return Ok(blog);
+    }
+
+
+    [HttpGet("by-user/{userId}")]
+    public async Task<IActionResult> GetBlogsByUserId(string userId, int page = 1, int pageSize = 10)
+    {
+        var blogs = await _blogService.GetBlogsByUserId(userId, page, pageSize);
+
+
+        var user = await _userService.GetById(userId);
+        string fullName = user != null ? $"{user.FirstName} {user.LastName}" : "Unknown User";
+
+
+        var blogDtos = blogs.Select(blog => new BlogReadDTO
+        {
+            Id = blog.Id!,
+            Title = blog.Title,
+            Content = blog.Content,
+            ImageUrls = blog.ImageUrls,
+            VideoUrls = blog.VideoUrls,
+            Category = blog.Category,
+            DateCreated = blog.DateCreated,
+            ViewCount = blog.ViewCount,
+            IsFeatured = blog.IsFeatured,
+            FullName = fullName
+        }).ToList();
+
+        return Ok(blogDtos);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteBlog(string id)
+    {
+        var success = await _blogService.Delete(id);
+        if (!success) return NotFound();
+        return NoContent();
+    }
+}
