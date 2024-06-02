4d623798ea8c6515efc46cc03a2c151e2e7c135c
emirkugic <emir.kugic@hotmail.com>
2024-06-02 11:10:11 +0200

    UPDATE: Refactored Program.cs and moved JWT and Swagger configuration into ServiceCollectionExtensions

--- Extensions/ServiceCollectionExtensions.cs
+++ Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,9 @@
 using nova_mas_blog_api.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 namespace nova_mas_blog_api.Extensions
 {
@@ -19,5 +23,74 @@ namespace nova_mas_blog_api.Extensions
 
             return services;
         }
+
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
+            return services;
+        }
+
+
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Nova Mas API",
+                    Version = "v1",
+                    Description = "API to manage blog application",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Emir Kugić",
+                        Email = "emirkugic0@gmail.com",
+                        Url = new Uri("https://emirkugic.tech/")
+                    }
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "bearer",
+                    In = ParameterLocation.Header,
+                },
+                new List<string>()
+            }
+                });
+            });
+
+            return services;
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Microsoft.OpenApi.Models;
 using nova_mas_blog_api.Data;
 using nova_mas_blog_api.Extensions;
 
@@ -13,70 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Connect to MongoDB
 builder.Services.AddSingleton<MongoDbContext>();
 
-// Extension methods from Extensions/ServiceCollectionExtensions.cs
+// Extension methods for Services
 builder.Services.AddCustomServices();
+
+// Extension method for AutoMapper
 builder.Services.AddCustomAutoMapper();
+
+// Extension method for JWT authentication
+builder.Services.AddJwtAuthentication(builder.Configuration);
+
+// Extension method for Swagger
+builder.Services.AddSwaggerDocumentation();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-
-// Add JWT Authentication
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ClockSkew = TimeSpan.Zero
-        };
-    });
-
-// Swagger configuration
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Nova Mas API",
-        Version = "v1",
-        Description = "API to manage blog application",
-        Contact = new OpenApiContact
-        {
-            Name = "Emir Kugić",
-            Email = "emirkugic0@gmail.com",
-            Url = new Uri("https://emirkugic.tech/")
-        }
-    });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                },
-                Scheme = "bearer",
-                In = ParameterLocation.Header,
-            },
-            new List<string>()
-        }
-    });
-});
 
 var app = builder.Build();
 
@@ -84,19 +27,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nova Mas API V1");
-        c.RoutePrefix = "swagger";
-    });
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nova Mas API V1"));
 }
 
 app.UseHttpsRedirection();
-
-// Enable authentication and authorization
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
